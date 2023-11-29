ae2094c35963f15df548994b9924266df44095ee
nGhart <naaghart@gmail.com>
2023-11-29 21:33:50 +0000

    dark card fix

--- tickets/src/components/ShowTicket.jsx
+++ tickets/src/components/ShowTicket.jsx
@@ -54,10 +54,11 @@ const ShowTicket = () => {
             href="#"
             className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100  dark:border-gray-700 dark:hover:bg-gray-700"
           >
-            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight">
               Ticket Details
             </h5>
-            <div className="font-normal text-gray-700 dark:text-gray-400">
+            <div className="font-normal">
+              {/* <div className="font-normal text-gray-700 dark:text-gray-400"> */}
               <div className="flex  justify-between">
                 <span>Type:</span>
                 <span>{ticket.ticket}</span>
