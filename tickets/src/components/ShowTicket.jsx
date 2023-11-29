b52b02f80715b649ffd56ccd1d9db2f83922603e
nGhart <naaghart@gmail.com>
2023-11-29 16:06:41 +0000

    fix classname

--- tickets/src/components/ShowTicket.jsx
+++ tickets/src/components/ShowTicket.jsx
@@ -51,12 +51,12 @@ const ShowTicket = () => {
 
           <a
             href="#"
-            class="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
+            className="block max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700"
           >
-            <h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
               Ticket Details
             </h5>
-            <div class="font-normal text-gray-700 dark:text-gray-400">
+            <div className="font-normal text-gray-700 dark:text-gray-400">
               <div className="flex  justify-between">
                 <span>Type:</span>
                 <span>{ticket.ticket}</span>
